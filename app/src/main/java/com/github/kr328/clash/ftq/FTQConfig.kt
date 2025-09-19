9d2cf6a80657543baee59605a83c4b659936e19f
xxmd <xxmd3720@gmail.com>
2025-09-19 18:26:20 +0800

    添加ftq配置类

---
+++ app/src/main/java/com/github/kr328/clash/ftq/FTQConfig.kt
@@ -0,0 +1,11 @@
+package com.github.kr328.clash.ftq
+
+object FTQConfig {
+
+    object DefaultProfile {
+        const val NAME = "FTQ_Android_Profile"
+        const val SOURCE = "https://ftq.ink/clash/config/android_test.yaml"
+    }
+
+}
+
